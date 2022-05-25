600f8615a53c5226fe24cb3511829b4b0af28b5c
Ckauark <ckauark@gmail.com>
2022-05-24 23:41:45 -0300

    adicionando o card

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const { name, price, image } = this.props;
+    return (
+      <div className="Card" data-testid="product">
+        <p>{ name }</p>
+        <img src={ image } alt="produto" />
+        <span>{ price }</span>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Card;
